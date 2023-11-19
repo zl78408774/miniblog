6c8f383026223a1ded213c64376c1ffc7f66a816
Long <long@macmini>
2023-11-19 20:48:12 +0800

    remove mini

--- app/models.py
+++ app/models.py
@@ -352,8 +352,8 @@ class BlogInfo(db.Model):
 
     @staticmethod
     def insert_blog_info():
-        blog_mini_info = BlogInfo(title=u'开源博客系统Blog_mini',
-                                  signature=u'让每个人都轻松拥有可管理的个人博客！— By xpleaf',
+        blog_mini_info = BlogInfo(title=u'开源博客系统Blog',
+                                  signature=u'让每个人都轻松拥有可管理的个人博客',
                                   navbar='inverse')
         db.session.add(blog_mini_info)
         db.session.commit()
